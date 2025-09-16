b239ba7d56eba360fc3bf87dcf61c24e80e70ca6
jbc-moe-inoue <inoue.ju22@jal.com>
2025-09-16 16:38:06 +0900

    fix: X/Oを直接Stateに持たせるよう修正 & squares.mapでループ描画に変更

--- src/App.js
+++ src/App.js
@@ -9,40 +9,31 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  const [xIsNext, setIsNext] = useState(true);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
-      return;
-    }
+    if (squares[i]) return;
+
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
+    nextSquares[i] = currentPlayer;
     setSquares(nextSquares);
-    setIsNext(!xIsNext);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   }
 
   return (
-    <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
+    <div className="board">
+      {[0, 1, 2].map(row => (
+        <div key={row} className="board-row">
+          {squares.slice(row * 3, row * 3 + 3).map((value, i) => (
+            <Square
+              key={row * 3 + i}
+              value={value}
+              onSquareClick={() => handleClick(row * 3 + i)}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
